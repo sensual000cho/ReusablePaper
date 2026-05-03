8b500d2c6671328708a93f52fe1976e0adf153c0
유능한친구\chh <loius0822yo@naver.com>
2026-05-03 23:51:02 +0900

    파일갱신

--- ReusablePaper/ReusablePaper.cpp
+++ ReusablePaper/ReusablePaper.cpp
@@ -3,7 +3,16 @@
 
 using namespace std;
 
-int solution(int num1, int num2) {
+int solution(int num1, int num2) 
+{
     int answer = 0;
+    if ((num1 - num2) == 0)
+    {
+        answer = 1;
+    }
+    else
+    {
+        answer = -1;
+    }
     return answer;
 }
